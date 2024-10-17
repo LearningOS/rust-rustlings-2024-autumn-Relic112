a33f4b6d7af20f829964fbdd7b9987e641dab0a6
Relic112 <165390467+Relic112@users.noreply.github.com>
2024-10-17 22:47:40 +0800

    Update algorithm1.rs

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -2,12 +2,13 @@
 	single linked list merge
 	This problem requires you to merge two ordered singly linked lists into one ordered singly linked list
 */
+//
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
 use std::vec::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Node<T> {
     val: T,
     next: Option<NonNull<Node<T>>>,
@@ -21,7 +22,7 @@ impl<T> Node<T> {
         }
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct LinkedList<T> {
     length: u32,
     start: Option<NonNull<Node<T>>>,
@@ -36,14 +37,14 @@ impl<T> Default for LinkedList<T> {
 
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
-
         Self {
             length: 0,
             start: None,
             end: None,
         }
-
     }
+}
+impl<T: PartialOrd + Clone + std::cmp::PartialOrd> LinkedList<T> {
 
     pub fn add(&mut self, obj: T) {
         let mut node = Box::new(Node::new(obj));
@@ -70,38 +71,46 @@ impl<T> LinkedList<T> {
             },
         }
     }
-	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
-	
-		//TODO
-{
-        let mut result = LinkedList::new();
-        let mut current_a = list_a.start;
-        let mut current_b = list_b.start;
 
-        while let (Some(node_a), Some(node_b)) = (current_a, current_b) {
-            unsafe {
-                if (*node_a.as_ptr()).val <= (*node_b.as_ptr()).val {
-                    result.add((*node_a.as_ptr()).val); 
-                    current_a = (*node_a.as_ptr()).next;
-                } else {
-                    result.add((*node_b.as_ptr()).val);  
-                    current_b = (*node_b.as_ptr()).next;
-                }
+    // 一个获取链表长度的方法
+    pub fn len(&mut self) -> i32 {
+        self.length.try_into().unwrap()  // 直接访问 length 字段
+    }
+
+	pub fn merge(mut list_a:LinkedList<T>,mut list_b:LinkedList<T>) -> Self {
+        let mut merged_list = LinkedList::new();
+        let mut a_current = list_a.start;
+        let mut b_current = list_b.start;
+
+        while a_current.is_some() || b_current.is_some() {
+            let a_next_val = a_current.map(|ptr| unsafe { &(*ptr.as_ptr()).val });
+            let b_next_val = b_current.map(|ptr| unsafe { &(*ptr.as_ptr()).val });
+
+            match (a_next_val, b_next_val) {
+                (Some(a_val), Some(b_val)) => {
+                    if a_val <= b_val {
+                        merged_list.add(a_val.clone());
+                        a_current = unsafe { (*a_current.unwrap().as_ptr()).next };
+                    } else {
+                        merged_list.add(b_val.clone());
+                        b_current = unsafe { (*b_current.unwrap().as_ptr()).next };
+                    }
+                },
+                (Some(a_val), None) => {
+                    merged_list.add(a_val.clone());
+                    a_current = unsafe { (*a_current.unwrap().as_ptr()).next };
+                },
+                (None, Some(b_val)) => {
+                    merged_list.add(b_val.clone());
+                    b_current = unsafe { (*b_current.unwrap().as_ptr()).next };
+                },
+                (None, None) => break,
             }
         }
 
-        let mut remaining = if current_a.is_some() { current_a } else { current_b };
-        while let Some(node) = remaining {
-            unsafe {
-                result.add((*node.as_ptr()).val);  
-                remaining = (*node.as_ptr()).next;
-            }
-        }
-
-        result
+        merged_list
     }
 }
-
 
 impl<T> Display for LinkedList<T>
 where
